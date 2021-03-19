9f161e76efb9a328d05767f2c326ed7825e37599
duttakapil <kapildutta28@gmail.com>
2021-03-19 12:59:48 +0530

    Added Optimistic UI for Comments

--- components/Comments.js
+++ components/Comments.js
@@ -3,6 +3,7 @@ import ms from 'ms'
 import useSWR from 'swr'
 import AddCommentBox from './AddCommentBox'
 import classNames from 'classnames'
+import { useSession } from 'next-auth/client'
 
 async function swrFetcher(path) {
     const res = await fetch(path)
@@ -10,22 +11,67 @@ async function swrFetcher(path) {
 }
 
 export default function Comments({slug}) {
+    const [session] = useSession();
     const {data: comments, mutate} = useSWR(`/api/comments?slug=${slug}`, swrFetcher)
 
-    const handleAddComment = async (content) => {
+    const createComment = async (fakeComment) => {
         const fetchRes = await fetch(`/api/comments?slug=${slug}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ content })
+            body: JSON.stringify({ content: fakeComment.content })
         })
-
+    
         if (!fetchRes.ok) {
-            alert(`Error: ${fetchRes.text()}`)
+            throw new Error(await fetchRes.text())
         }
+    
+        const addedComment = await fetchRes.json()
+        mutate((currentComments) => {
+            const newComments = [];
+            for (const c of currentComments) {
+                if (c.id === fakeComment.id){
+                    newComments.push(addedComment)
+                } else {
+                    newComments.push(c);
+                }
+            }
+            return newComments
+        }, false)
+    }
+    
+    const handleAddComment = async (content) => {
+        const fakeComment = {
+            id: Math.random(),
+            userId: session.user.id,
+            name: session.user.profile.name,
+            avatar: session.user.profile.avatar,
+            content,
+            createdAt: Date.now(),
+            clientOnly: true
+        }
+    
+        mutate([...comments, fakeComment], false)
 
-        mutate()
+        createComment(fakeComment)
+            .catch(err => {
+                mutate(currentComments => {
+                    const newComments = []
+                    for (const c of currentComments) {
+                        if (c.id === fakeComment.id) {
+                            newComments.push({
+                                ...fakeComment,
+                                error: err.message
+                            })
+                        } else {
+                            newComments.push(c)
+                        }
+                    }
+        
+                    return newComments
+                }, false)
+            })    
     }
 
     if (!comments) {
